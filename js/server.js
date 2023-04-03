1249ef31fb29c020761cb035a19de1311aeab9f9
Matt Visnovsky <matt@visnovsky.us>
2023-04-03 13:07:08 -0600

    Functional animation between hex and modal

--- js/server.js
+++ js/server.js
@@ -17,19 +17,69 @@ $(document).ready(function () {
 
 });
 
-
+var $clone;
 function openServerDetails(hex, serverName) {
-    console.log(hex, $(hex).find('.hex_inner'))
-    var time = .5;
+    const animTime = 500;// Animation time in milliseconds
+    const $hexParent = $(hex).parent();// Cache selectors
+    const $hexInner = $(hex).find('.hex_inner');
+    const $container = $('.hexagons');
 
-    $(hex).parent()
-        .toggleClass('centered')
-        .css('transition', `all ${time}s ease-in-out`);
-    $(hex).find('.hex_inner')
-        .toggleClass('squared')
-        .css('transition', `all ${time}s ease-in-out`)
-        .off('mouseenter mouseleave')
+    // Modified flipForward and flibBack functions to add "third flip" when clicked
+    if ($hexInner.hasClass('squared')) {// Hex is in square mode
+        $hexParent.replaceWith($clone);
+        $clone.find('.hex_inner').on('mouseenter', function() {
+            flipForward($clone, animTime, null);
+        }).on('mouseleave', function() {
+            flipBack($clone, animTime);
+        });
+    } else {// Else needs to be squared
+        $clone = $hexParent.clone()// Clone the node
 
-    console.log(serverName);
+        $hexParent.css({
+            position: 'fixed', // Change to fixed for absolute centering
+            width: '100%',
+            height: '100%',
+            left: 0, // Set the left position to the center
+            top: 0, // Set the top position to the center
+            'z-index': 1,
+            transition: `all ${animTime}ms ease-in-out`,
+        })
+
+        $(hex).css({
+            width: '100%',
+            height: '100%',
+        });
+
+        $hexInner.toggleClass('squared')
+            .css({
+                width: '100%',
+                height: '100%',
+                transition: `all ${animTime}ms ease-in-out`,
+            })
+            .off('mouseenter mouseleave')
+
+        if ($hexParent.hasClass('flipped')) {// Flip Back
+
+            $hexParent.addClass('flip-back')
+            setTimeout(function () {
+                $hexParent.find('.inner-text-flipped').css('visibility', 'hidden');
+                setTimeout(function () {
+                    $hexParent.removeClass('flipping flipped flip-back');
+                }.bind(this), animTime / 2);
+            }.bind(this), animTime / 2);
+
+        } else {// Flip Forward
+
+            $hexParent.addClass('flipping');
+            setTimeout(function () {
+                $hexParent.find('.inner-title').hide();
+                $hexParent.find('.inner-text-flipped').css('visibility', 'hidden');
+                setTimeout(function () {
+                    $hexParent.addClass('flipped');
+                }.bind(this), animTime / 2);
+            }.bind(this), animTime / 2);
+        }
+
+    }
 
 }
