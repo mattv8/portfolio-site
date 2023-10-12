d9d6a85e8b4d2d99ec6bdcb8213e0ec0077c45ec
Matt Visnovsky <matt@visnovsky.us>
2023-10-12 05:04:14 -0600

    Mobile friendliness flip

--- js/functions.js
+++ js/functions.js
@@ -7,72 +7,76 @@ var rotations = 0;
 
 function goToPage(page, replaceSelector, _this) {
 
+	const $hexParent = $(_this).parent();
+
   // Redirect to root if page is null
   if (!page) { page = GLOBAL.config.default_page; }
 
   // Else route to specified page asynchronously
-  $.ajax({
-    url: 'index.php',
-    type: 'GET',
-    data: { page: page },
-    beforeSend: function () {// Start loading animation
-      pageLoaded = false;
-      rotations = 0;// Reset rotations
-      if (_this) { rotate(_this, .5, 2); }
-    },
-    complete: function () {// Stop loading animation
-      pageLoaded = true;// Stop the rotate function with flag
-    },
-    success: function (response) {
-      let selector;
-      if (replaceSelector) {
-        selector = document.getElementById(replaceSelector);
-        selector.style.transition = 'opacity 500ms';
-        selector.style.opacity = 0;
-      } else {
-        document.body.style.transition = 'opacity 500ms';
-        document.body.style.opacity = 0;
-      }
-      setTimeout(function () {
+  if ($hexParent.hasClass('flipped')) {
+    $.ajax({
+      url: 'index.php',
+      type: 'GET',
+      data: { page: page },
+      beforeSend: function () {// Start loading animation
+        pageLoaded = false;
+        rotations = 0;// Reset rotations
+        if (_this) { rotate(_this, .5, 2); }
+      },
+      complete: function () {// Stop loading animation
+        pageLoaded = true;// Stop the rotate function with flag
+      },
+      success: function (response) {
+        let selector;
         if (replaceSelector) {
-          var parser = new DOMParser();
-          var newDoc = parser.parseFromString(response, 'text/html');
-          var container = newDoc.getElementById(replaceSelector);
-          selector.innerHTML = container.innerHTML;
-
-          var scripts = container.getElementsByTagName('script');
-          for (var i = 0; i < scripts.length; i++) {
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = scripts[i].src;
-            selector.appendChild(script);
-          }
-
+          selector = document.getElementById(replaceSelector);
           selector.style.transition = 'opacity 500ms';
-          selector.style.opacity = 1;
+          selector.style.opacity = 0;
         } else {
-          var parser = new DOMParser();
-          var newDoc = parser.parseFromString(response, 'text/html');
-          document.head.innerHTML = newDoc.head.innerHTML;
-          document.body.innerHTML = newDoc.body.innerHTML;
-
-          var scripts = newDoc.getElementsByTagName('script');
-          for (var i = 0; i < scripts.length; i++) {
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = scripts[i].src;
-            document.body.appendChild(script);
-          }
-
           document.body.style.transition = 'opacity 500ms';
-          document.body.style.opacity = 1;
+          document.body.style.opacity = 0;
         }
-      }, 500);
+        setTimeout(function () {
+          if (replaceSelector) {
+            var parser = new DOMParser();
+            var newDoc = parser.parseFromString(response, 'text/html');
+            var container = newDoc.getElementById(replaceSelector);
+            selector.innerHTML = container.innerHTML;
 
-      var popstate = (page === GLOBAL.config.default_page) ? '/' : `/?page=${page}`;
-      history.pushState(page, null, popstate);// add the page to the browser's history
-    },
-  });
+            var scripts = container.getElementsByTagName('script');
+            for (var i = 0; i < scripts.length; i++) {
+              var script = document.createElement('script');
+              script.type = 'text/javascript';
+              script.src = scripts[i].src;
+              selector.appendChild(script);
+            }
+
+            selector.style.transition = 'opacity 500ms';
+            selector.style.opacity = 1;
+          } else {
+            var parser = new DOMParser();
+            var newDoc = parser.parseFromString(response, 'text/html');
+            document.head.innerHTML = newDoc.head.innerHTML;
+            document.body.innerHTML = newDoc.body.innerHTML;
+
+            var scripts = newDoc.getElementsByTagName('script');
+            for (var i = 0; i < scripts.length; i++) {
+              var script = document.createElement('script');
+              script.type = 'text/javascript';
+              script.src = scripts[i].src;
+              document.body.appendChild(script);
+            }
+
+            document.body.style.transition = 'opacity 500ms';
+            document.body.style.opacity = 1;
+          }
+        }, 500);
+
+        var popstate = (page === GLOBAL.config.default_page) ? '/' : `/?page=${page}`;
+        history.pushState(page, null, popstate);// add the page to the browser's history
+      },
+    });
+  }
 }
 
 
