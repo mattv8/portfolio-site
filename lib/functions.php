7e5990da7669f2e18368dccce8f2487041347a4f
Matt Visnovsky <matt@visnovsky.us>
2023-11-14 12:21:48 -0700

    Autoformat

--- lib/functions.php
+++ lib/functions.php
@@ -5,56 +5,55 @@
 #==============================================================================
 
 # Requests an API ticket.
-function pveTicket($credentials,$oldTicket) {
-    
+function pveTicket($credentials, $oldTicket)
+{
+
     $pveUN = $credentials['pveUN'];
     $pvePW = $credentials['pvePW'];
 
-    if ( empty($oldTicket['time']) or (time() - $oldTicket['time'] > 7200) ) {// Two hours have elapsed or ticket doesn't exist
-        
+    if (empty($oldTicket['time']) or (time() - $oldTicket['time'] > 7200)) { // Two hours have elapsed or ticket doesn't exist
+
         $newTicket = '';
-        $ch = curl_init();// create a new cURL resource
+        $ch = curl_init(); // create a new cURL resource
         $options = array(
             CURLOPT_POSTFIELDS => "username=$pveUN&password=$pvePW",
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_SSL_VERIFYHOST => false,
             CURLOPT_SSL_VERIFYPEER => false,
             CURLOPT_TIMEOUT => 10,
-            CURLOPT_URL => $credentials['pveURL']."/api2/json/access/ticket",
+            CURLOPT_URL => $credentials['pveURL'] . "/api2/json/access/ticket",
         );
-        curl_setopt_array($ch, $options);// set URL and other appropriate options
-    
-        $result = curl_exec($ch);// execute the request
-    
-        curl_close($ch);// close cURL resource, and free up system resources
+        curl_setopt_array($ch, $options); // set URL and other appropriate options
+
+        $result = curl_exec($ch); // execute the request
+
+        curl_close($ch); // close cURL resource, and free up system resources
         if ($result === false) {
             echo "cURL Error: " . curl_error($ch);
         } else {
-            $response = json_decode($result,true);// do something with the response
+            $response = json_decode($result, true); // do something with the response
             $newTicket = $response['data']['ticket'];
-            updateTicket($newTicket,'pve_ticket');
+            updateTicket($newTicket, 'pve_ticket');
         }
         return $newTicket;
+    } else { // Less than two hours have elapsed since the last modification
 
-    } else {// Less than two hours have elapsed since the last modification
-        
         return $oldTicket['ticket'];
-    
     }
-
 }
 
 # Saves the ticket to a config file
-function updateTicket($newTicket,$var) {
-    $path = $_SERVER['DOCUMENT_ROOT'].'/conf/config.local.php';
+function updateTicket($newTicket, $var)
+{
+    $path = $_SERVER['DOCUMENT_ROOT'] . '/conf/config.local.php';
     $config = file_get_contents($path); // Read the contents of the config.local.php file
 
     // Use a single regex pattern to match both the 'ticket' and 'time' values
     $pattern = "/\\$$var\s*=\s*array\(\s*'ticket' => '(.*)',\s*'time' => '(.*)'\s*\);/i";
 
     // Use a callback function to perform the replacements
-    $replacement = function($matches) use ($newTicket,$var) {
-        return "\$$var = array(\n\t'ticket' => '$newTicket',\n\t'time' => '".time()."'\n);";
+    $replacement = function ($matches) use ($newTicket, $var) {
+        return "\$$var = array(\n\t'ticket' => '$newTicket',\n\t'time' => '" . time() . "'\n);";
     };
 
     // Perform all the replacements at once using preg_replace_callback
@@ -66,32 +65,34 @@ function updateTicket($newTicket,$var) {
 
 # Utility function to query the PVE API
 # See: https://pve.proxmox.com/pve-docs/api-viewer/
-function pveQuery($pveURL, $ticket, $path) {
+function pveQuery($pveURL, $ticket, $path)
+{
 
     $response = array();
-    $ch = curl_init();// create a new cURL resource
+    $ch = curl_init(); // create a new cURL resource
     $options = array(
-        CURLOPT_URL => $pveURL.'/api2/json/'.$path,
+        CURLOPT_URL => $pveURL . '/api2/json/' . $path,
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_SSL_VERIFYPEER => false,
         CURLOPT_SSL_VERIFYHOST => false,
-        CURLOPT_COOKIE => "PVEAuthCookie=".$ticket
+        CURLOPT_COOKIE => "PVEAuthCookie=" . $ticket
     );
     curl_setopt_array($ch, $options);
 
-    $result = curl_exec($ch);// execute the request
+    $result = curl_exec($ch); // execute the request
 
-    curl_close($ch);// close cURL resource, and free up system resources
+    curl_close($ch); // close cURL resource, and free up system resources
 
     if ($result === false) {
         echo "cURL Error: " . curl_error($ch);
     } else {
-        $response = json_decode($result,true);// do something with the response
+        $response = json_decode($result, true); // do something with the response
     }
     return $response['data'];
 }
 
-function secondsToTime($seconds) {
+function secondsToTime($seconds)
+{
     $dtF = new \DateTime('@0');
     $dtT = new \DateTime("@$seconds");
     return $dtF->diff($dtT)->format('%ad %hh %im');
