5256dd8889443819fb2cb50e13454978231e2f66
Matt Visnovsky <matt.visnovsk@gmail.com>
2025-01-12 14:10:55 -0700

    vscode tasks updates

--- .vscode/start.sh
+++ .vscode/start.sh
@@ -3,12 +3,6 @@
 env_dir=. # .env directory
 docker_dir=$env_dir # docker-compose.yml directory
 
-# Ensure Docker is running
-if ! systemctl is-active --quiet docker; then
-    echo "Starting Docker daemon..."
-    sudo systemctl start docker
-fi
-
 # Start Docker Compose services
 echo "Starting Docker Compose services..."
 sudo docker compose --env-file $env_dir/.env -f $docker_dir/docker-compose.yml up --build
